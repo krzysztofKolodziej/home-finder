ea787193dba121d1cdbf5511a5f1b6315aff3d76
Krzysztof Kolodziej <krzysztof.kolodziej2212@gmail.com>
2024-09-01 23:06:17 +0200

    Remove set method from CreditCalculatorDto
    and update unit test, because Dto should be immutable

--- src/test/java/com/aplication/homeFinder/creditCalculator/service/CalculatorTest.java
+++ src/test/java/com/aplication/homeFinder/creditCalculator/service/CalculatorTest.java
@@ -18,155 +18,327 @@ class CalculatorTest {
 
     @Test
     void shouldReturnZeroWhenDelayInLoanRepaymentIsTrue() {
-        CreditCalculatorDto creditCalculatorDtoTest = getCreditCalculatorDto();
-        creditCalculatorDtoTest.setDelayInLoanRepayment(true);
+        CreditCalculatorDto creditCalculatorDtoTest = CreditCalculatorDto.builder()
+                .propertyValue(825000)
+                .downPayment(200000)
+                .creditAmount(625000)
+                .repaymentPeriod(30)
+                .sourceOfIncome(SourceOfIncome.ORDER_CONTRACT)
+                .contractDurationInMonth(34)
+                .continuityOfEmployment(true)
+                .monthlyNetIncome(12000)
+                .monthlyExpenditures(3000)
+                .delayInLoanRepayment(true)
+                .numberOfDependents(2)
+                .monthlyAmountOtherLoans(500)
+                .creditCardLimit(10000)
+                .dateOfBirth(LocalDate.of(1985, 1, 15))
+                .phoneNumber("123456789")
+                .name("Jan")
+                .build();
+
         int result = calculator.maxLoanAmount(creditCalculatorDtoTest);
         assertEquals(0, result);
     }
 
     @Test
     void shouldReturnCreditWorthinessWhenFixedTermContractIsGreaterThanTwelve() {
-        CreditCalculatorDto creditCalculatorDtoTest = getCreditCalculatorDto();
-        creditCalculatorDtoTest.setSourceOfIncome(SourceOfIncome.FIXED_TERM_CONTRACT);
-        creditCalculatorDtoTest.setContractDurationInMonth(13);
+        CreditCalculatorDto creditCalculatorDtoTest = CreditCalculatorDto.builder()
+                .propertyValue(825000)
+                .downPayment(200000)
+                .creditAmount(625000)
+                .repaymentPeriod(30)
+                .sourceOfIncome(SourceOfIncome.FIXED_TERM_CONTRACT)
+                .contractDurationInMonth(13)
+                .continuityOfEmployment(true)
+                .monthlyNetIncome(12000)
+                .monthlyExpenditures(3000)
+                .delayInLoanRepayment(false)
+                .numberOfDependents(2)
+                .monthlyAmountOtherLoans(500)
+                .creditCardLimit(10000)
+                .dateOfBirth(LocalDate.of(1985, 1, 15))
+                .phoneNumber("123456789")
+                .name("Jan")
+                .build();
+
         int result = calculator.maxLoanAmount(creditCalculatorDtoTest);
         assertNotEquals(0,result);
     }
 
     @Test
     void shouldReturnZeroWhenFixedTermContractIsShorterOrEqualsTwelve() {
-        CreditCalculatorDto creditCalculatorDtoTest = getCreditCalculatorDto();
-        creditCalculatorDtoTest.setSourceOfIncome(SourceOfIncome.FIXED_TERM_CONTRACT);
-        creditCalculatorDtoTest.setContractDurationInMonth(12);
+        CreditCalculatorDto creditCalculatorDtoTest = CreditCalculatorDto.builder()
+                .propertyValue(825000)
+                .downPayment(200000)
+                .creditAmount(625000)
+                .repaymentPeriod(30)
+                .sourceOfIncome(SourceOfIncome.FIXED_TERM_CONTRACT)
+                .contractDurationInMonth(12)
+                .continuityOfEmployment(true)
+                .monthlyNetIncome(12000)
+                .monthlyExpenditures(3000)
+                .delayInLoanRepayment(false)
+                .numberOfDependents(2)
+                .monthlyAmountOtherLoans(500)
+                .creditCardLimit(10000)
+                .dateOfBirth(LocalDate.of(1985, 1, 15))
+                .phoneNumber("123456789")
+                .name("Jan")
+                .build();
+
         int result = calculator.maxLoanAmount(creditCalculatorDtoTest);
         assertEquals(0, result);
     }
 
     @Test
     void shouldReturnZeroWhenSourceOfIncomeEqualsBusinessOperationLessThanYear() {
-        CreditCalculatorDto creditCalculatorDtoTest = getCreditCalculatorDto();
-        creditCalculatorDtoTest.setSourceOfIncome(SourceOfIncome.BUSINESS_OPERATION_LESS_THAN_YEAR);
+        CreditCalculatorDto creditCalculatorDtoTest = CreditCalculatorDto.builder()
+                .propertyValue(825000)
+                .downPayment(200000)
+                .creditAmount(625000)
+                .repaymentPeriod(30)
+                .sourceOfIncome(SourceOfIncome.BUSINESS_OPERATION_LESS_THAN_YEAR)
+                .contractDurationInMonth(34)
+                .continuityOfEmployment(true)
+                .monthlyNetIncome(12000)
+                .monthlyExpenditures(3000)
+                .delayInLoanRepayment(false)
+                .numberOfDependents(2)
+                .monthlyAmountOtherLoans(500)
+                .creditCardLimit(10000)
+                .dateOfBirth(LocalDate.of(1985, 1, 15))
+                .phoneNumber("123456789")
+                .name("Jan")
+                .build();
+
         int result = calculator.maxLoanAmount(creditCalculatorDtoTest);
         assertEquals(0, result);
     }
 
     @Test
     void shouldReturnZeroWhenSourceOfIncomeEqualsContractOfSpecificWorkAndContinuityOfEmploymentEqualsFalse() {
-        CreditCalculatorDto creditCalculatorDtoTest = getCreditCalculatorDto();
-        creditCalculatorDtoTest.setSourceOfIncome(SourceOfIncome.CONTRACT_OF_SPECIFIC_WORK);
-        creditCalculatorDtoTest.setContinuityOfEmployment(false);
+        CreditCalculatorDto creditCalculatorDtoTest = CreditCalculatorDto.builder()
+                .propertyValue(825000)
+                .downPayment(200000)
+                .creditAmount(625000)
+                .repaymentPeriod(30)
+                .sourceOfIncome(SourceOfIncome.CONTRACT_OF_SPECIFIC_WORK)
+                .contractDurationInMonth(34)
+                .continuityOfEmployment(false)
+                .monthlyNetIncome(12000)
+                .monthlyExpenditures(3000)
+                .delayInLoanRepayment(false)
+                .numberOfDependents(2)
+                .monthlyAmountOtherLoans(500)
+                .creditCardLimit(10000)
+                .dateOfBirth(LocalDate.of(1985, 1, 15))
+                .phoneNumber("123456789")
+                .name("Jan")
+                .build();
+
         int result = calculator.maxLoanAmount(creditCalculatorDtoTest);
         assertEquals(0, result);
     }
 
     @Test
     void shouldReturnZeroWhenSourceOfIncomeEqualsOrderContractAndContinuityOfEmploymentEqualsFalse() {
-        CreditCalculatorDto creditCalculatorDtoTest = getCreditCalculatorDto();
-        creditCalculatorDtoTest.setSourceOfIncome(SourceOfIncome.ORDER_CONTRACT);
-        creditCalculatorDtoTest.setContinuityOfEmployment(false);
+        CreditCalculatorDto creditCalculatorDtoTest = CreditCalculatorDto.builder()
+                .propertyValue(825000)
+                .downPayment(200000)
+                .creditAmount(625000)
+                .repaymentPeriod(30)
+                .sourceOfIncome(SourceOfIncome.ORDER_CONTRACT)
+                .contractDurationInMonth(34)
+                .continuityOfEmployment(false)
+                .monthlyNetIncome(12000)
+                .monthlyExpenditures(3000)
+                .delayInLoanRepayment(false)
+                .numberOfDependents(2)
+                .monthlyAmountOtherLoans(500)
+                .creditCardLimit(10000)
+                .dateOfBirth(LocalDate.of(1985, 1, 15))
+                .phoneNumber("123456789")
+                .name("Jan")
+                .build();
+
         int result = calculator.maxLoanAmount(creditCalculatorDtoTest);
         assertEquals(0, result);
     }
 
     @Test
     void shouldReturnZeroWhenSourceOfIncomeEqualsAnnuitiesAndContinuityOfEmploymentEqualsFalse() {
-        CreditCalculatorDto creditCalculatorDtoTest = getCreditCalculatorDto();
-        creditCalculatorDtoTest.setSourceOfIncome(SourceOfIncome.ANNUITIES);
-        creditCalculatorDtoTest.setContinuityOfEmployment(false);
+        CreditCalculatorDto creditCalculatorDtoTest = CreditCalculatorDto.builder()
+                .propertyValue(825000)
+                .downPayment(200000)
+                .creditAmount(625000)
+                .repaymentPeriod(30)
+                .sourceOfIncome(SourceOfIncome.ANNUITIES)
+                .contractDurationInMonth(34)
+                .continuityOfEmployment(false)
+                .monthlyNetIncome(12000)
+                .monthlyExpenditures(3000)
+                .delayInLoanRepayment(false)
+                .numberOfDependents(2)
+                .monthlyAmountOtherLoans(500)
+                .creditCardLimit(10000)
+                .dateOfBirth(LocalDate.of(1985, 1, 15))
+                .phoneNumber("123456789")
+                .name("Jan")
+                .build();
+
         int result = calculator.maxLoanAmount(creditCalculatorDtoTest);
         assertEquals(0, result);
     }
 
     @Test
     void shouldReturnZeroWhenTotalMonthlyDebtsIsGreaterThanMonthlyNetIncome() {
-        CreditCalculatorDto creditCalculatorDtoTest = getCreditCalculatorDto();
-        creditCalculatorDtoTest.setCreditCardLimit(20000);
-        creditCalculatorDtoTest.setMonthlyExpenditures(10000);
-        creditCalculatorDtoTest.setMonthlyAmountOtherLoans(3000); // Total cost 13400
-        creditCalculatorDtoTest.setMonthlyNetIncome(10000);
+        CreditCalculatorDto creditCalculatorDtoTest = CreditCalculatorDto.builder()
+                .propertyValue(825000)
+                .downPayment(200000)
+                .creditAmount(625000)
+                .repaymentPeriod(30)
+                .sourceOfIncome(SourceOfIncome.ORDER_CONTRACT)
+                .contractDurationInMonth(34)
+                .continuityOfEmployment(true)
+                .monthlyNetIncome(10000)
+                .monthlyExpenditures(10000)
+                .delayInLoanRepayment(false)
+                .numberOfDependents(2)
+                .monthlyAmountOtherLoans(3000)
+                .creditCardLimit(20000) // Total cost 13400
+                .dateOfBirth(LocalDate.of(1985, 1, 15))
+                .phoneNumber("123456789")
+                .name("Jan")
+                .build();
+
         int result = calculator.maxLoanAmount(creditCalculatorDtoTest);
         assertEquals(0, result);
     }
 
     @Test
     void shouldReturnZeroWhenTotalMonthlyDebtsIsGreaterThanMonthlyNetIncomeAndNumberOfDependentsEqualsSix() {
-        CreditCalculatorDto creditCalculatorDtoTest = getCreditCalculatorDto();
-        creditCalculatorDtoTest.setCreditCardLimit(20000);
-        creditCalculatorDtoTest.setMonthlyExpenditures(5000);
-        creditCalculatorDtoTest.setNumberOfDependents(6);
-        creditCalculatorDtoTest.setMonthlyAmountOtherLoans(3000); // Total cost 6900
-        creditCalculatorDtoTest.setMonthlyNetIncome(10000); // Income reduced by Max_Dti 6000
+        CreditCalculatorDto creditCalculatorDtoTest = CreditCalculatorDto.builder()
+                .propertyValue(825000)
+                .downPayment(200000)
+                .creditAmount(625000)
+                .repaymentPeriod(30)
+                .sourceOfIncome(SourceOfIncome.ORDER_CONTRACT)
+                .contractDurationInMonth(34)
+                .continuityOfEmployment(true)
+                .monthlyNetIncome(10000) // Income reduced by Max_Dti 6000
+                .monthlyExpenditures(5000)
+                .delayInLoanRepayment(false)
+                .numberOfDependents(6)
+                .monthlyAmountOtherLoans(3000)
+                .creditCardLimit(20000) // Total cost 6900
+                .dateOfBirth(LocalDate.of(1985, 1, 15))
+                .phoneNumber("123456789")
+                .name("Jan")
+                .build();
+
         int result = calculator.maxLoanAmount(creditCalculatorDtoTest);
         assertEquals(0, result);
     }
 
     @Test
     void shouldReturnZeroWhenTotalMonthlyDebtsIsGreaterThanMonthlyNetIncomeAndNumberOfDependentsEqualsTen() {
-        CreditCalculatorDto creditCalculatorDtoTest = getCreditCalculatorDto();
-        creditCalculatorDtoTest.setCreditCardLimit(20000);
-        creditCalculatorDtoTest.setMonthlyExpenditures(5000);
-        creditCalculatorDtoTest.setNumberOfDependents(10);
-        creditCalculatorDtoTest.setMonthlyAmountOtherLoans(3000); // Total cost 7150
-        creditCalculatorDtoTest.setMonthlyNetIncome(10000); // Income reduced by Max_Dti 6000
+        CreditCalculatorDto creditCalculatorDtoTest = CreditCalculatorDto.builder()
+                .propertyValue(825000)
+                .downPayment(200000)
+                .creditAmount(625000)
+                .repaymentPeriod(30)
+                .sourceOfIncome(SourceOfIncome.ORDER_CONTRACT)
+                .contractDurationInMonth(34)
+                .continuityOfEmployment(true)
+                .monthlyNetIncome(10000) // Income reduced by Max_Dti 6000
+                .monthlyExpenditures(5000)
+                .delayInLoanRepayment(false)
+                .numberOfDependents(10)
+                .monthlyAmountOtherLoans(3000)
+                .creditCardLimit(20000) // Total cost 7150
+                .dateOfBirth(LocalDate.of(1985, 1, 15))
+                .phoneNumber("123456789")
+                .name("Jan")
+                .build();
+
         int result = calculator.maxLoanAmount(creditCalculatorDtoTest);
         assertEquals(0, result);
     }
 
     @Test
     void shouldReturnCreditWorthinessWhenTotalMonthlyDebtsIsLessThanMonthlyNetIncomeAndNumberOfDependentsEqualsZero() {
-        CreditCalculatorDto creditCalculatorDtoTest = getCreditCalculatorDto();
-        creditCalculatorDtoTest.setCreditCardLimit(20000);
-        creditCalculatorDtoTest.setMonthlyExpenditures(5000);
-        creditCalculatorDtoTest.setNumberOfDependents(0);
-        creditCalculatorDtoTest.setMonthlyAmountOtherLoans(3000); // Total cost 4400
-        creditCalculatorDtoTest.setMonthlyNetIncome(10000); // Income reduced by Max_Dti 6000
+        CreditCalculatorDto creditCalculatorDtoTest = CreditCalculatorDto.builder()
+                .propertyValue(825000)
+                .downPayment(200000)
+                .creditAmount(625000)
+                .repaymentPeriod(30)
+                .sourceOfIncome(SourceOfIncome.ORDER_CONTRACT)
+                .contractDurationInMonth(34)
+                .continuityOfEmployment(true)
+                .monthlyNetIncome(10000) // Income reduced by Max_Dti 6000
+                .monthlyExpenditures(5000)
+                .delayInLoanRepayment(false)
+                .numberOfDependents(0)
+                .monthlyAmountOtherLoans(3000)
+                .creditCardLimit(20000) // Total cost 4400
+                .dateOfBirth(LocalDate.of(1985, 1, 15))
+                .phoneNumber("123456789")
+                .name("Jan")
+                .build();
+
         int result = calculator.maxLoanAmount(creditCalculatorDtoTest);
         assertNotEquals(0, result);
     }
 
     @Test
     void shouldReturnCreditWorthinessWhenTotalMonthlyDebtsIsLessThanMonthlyNetIncomeAndNumberOfDependentsEqualsOne() {
-        CreditCalculatorDto creditCalculatorDtoTest = getCreditCalculatorDto();
-        creditCalculatorDtoTest.setCreditCardLimit(20000);
-        creditCalculatorDtoTest.setMonthlyExpenditures(5000);
-        creditCalculatorDtoTest.setNumberOfDependents(1);
-        creditCalculatorDtoTest.setMonthlyAmountOtherLoans(3000); // Total cost 4900
-        creditCalculatorDtoTest.setMonthlyNetIncome(10000); // Income reduced by Max_Dti 6000
+        CreditCalculatorDto creditCalculatorDtoTest = CreditCalculatorDto.builder()
+                .propertyValue(825000)
+                .downPayment(200000)
+                .creditAmount(625000)
+                .repaymentPeriod(30)
+                .sourceOfIncome(SourceOfIncome.ORDER_CONTRACT)
+                .contractDurationInMonth(34)
+                .continuityOfEmployment(true)
+                .monthlyNetIncome(10000) // Income reduced by Max_Dti 6000
+                .monthlyExpenditures(5000)
+                .delayInLoanRepayment(false)
+                .numberOfDependents(1)
+                .monthlyAmountOtherLoans(3000) // Total cost 4900
+                .creditCardLimit(20000)
+                .dateOfBirth(LocalDate.of(1985, 1, 15))
+                .phoneNumber("123456789")
+                .name("Jan")
+                .build();
+
         int result = calculator.maxLoanAmount(creditCalculatorDtoTest);
         assertNotEquals(0, result);
     }
 
     @Test
     void shouldReturnCreditWorthinessWhenTotalMonthlyDebtsIsLessThanMonthlyNetIncomeAndNumberOfDependentsEqualsThree() {
-        CreditCalculatorDto creditCalculatorDtoTest = getCreditCalculatorDto();
-        creditCalculatorDtoTest.setCreditCardLimit(20000);
-        creditCalculatorDtoTest.setMonthlyExpenditures(5000);
-        creditCalculatorDtoTest.setNumberOfDependents(3);
-        creditCalculatorDtoTest.setMonthlyAmountOtherLoans(3000); // Total cost 5900
-        creditCalculatorDtoTest.setMonthlyNetIncome(10000); // Income reduced by Max_Dti 6000
+        CreditCalculatorDto creditCalculatorDtoTest = CreditCalculatorDto.builder()
+                .propertyValue(825000)
+                .downPayment(200000)
+                .creditAmount(625000)
+                .repaymentPeriod(30)
+                .sourceOfIncome(SourceOfIncome.ORDER_CONTRACT)
+                .contractDurationInMonth(34)
+                .continuityOfEmployment(true)
+                .monthlyNetIncome(10000) // Income reduced by Max_Dti 6000
+                .monthlyExpenditures(5000)
+                .delayInLoanRepayment(false)
+                .numberOfDependents(3)
+                .monthlyAmountOtherLoans(3000)
+                .creditCardLimit(20000) // Total cost 5900
+                .dateOfBirth(LocalDate.of(1985, 1, 15))
+                .phoneNumber("123456789")
+                .name("Jan")
+                .build();
+
         int result = calculator.maxLoanAmount(creditCalculatorDtoTest);
         assertNotEquals(0, result);
-    }
-
-    private static CreditCalculatorDto getCreditCalculatorDto() {
-        CreditCalculatorDto creditCalculatorDto = new CreditCalculatorDto();
-        creditCalculatorDto.setPropertyValue(825000);
-        creditCalculatorDto.setDownPayment(200000);
-        creditCalculatorDto.setCreditAmount(625000);
-        creditCalculatorDto.setRepaymentPeriod(30);
-        creditCalculatorDto.setSourceOfIncome(SourceOfIncome.ORDER_CONTRACT);
-        creditCalculatorDto.setContractDurationInMonth(34);
-        creditCalculatorDto.setContinuityOfEmployment(true);
-        creditCalculatorDto.setMonthlyNetIncome(12000);
-        creditCalculatorDto.setMonthlyExpenditures(3000);
-        creditCalculatorDto.setDelayInLoanRepayment(false);
-        creditCalculatorDto.setNumberOfDependents(2);
-        creditCalculatorDto.setMonthlyAmountOtherLoans(500);
-        creditCalculatorDto.setCreditCardLimit(10000);
-        creditCalculatorDto.setDateOfBirth(LocalDate.of(1985, 1, 15));
-        creditCalculatorDto.setPhoneNumber("123456789");
-        creditCalculatorDto.setName("Jan");
-        return creditCalculatorDto;
     }
 
 }
